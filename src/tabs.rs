b714f2a7cbecc352e57558e8899ec5321fc0dedc
Lej77 <31554212+Lej77@users.noreply.github.com>
2025-11-12 04:35:06 +0100

    fix: scrolling and layout issues with tab bar

--- src/tabs.rs
+++ src/tabs.rs
@@ -1,7 +1,7 @@
 use gpui::{
     AlignItems, Context, Empty, InteractiveElement, IntoElement, MouseButton, MouseDownEvent,
-    ParentElement, Point, Render, ScrollHandle, ScrollWheelEvent, SharedString, StyleRefinement,
-    Styled, Window, div, px,
+    ParentElement, Pixels, Point, Render, ScrollHandle, ScrollWheelEvent, SharedString,
+    StyleRefinement, Styled, Window, div, px,
 };
 use gpui_component::button::Button;
 use gpui_component::tab::{Tab, TabBar};
@@ -32,6 +32,7 @@ pub struct TabsView<T: 'static> {
     active_tab: usize,
     tabs: Vec<Option<T>>,
     scroll_handle: ScrollHandle,
+    latest_scroll_offset: Point<Pixels>,
     on_tab_changed: Box<dyn Fn(&mut Window, &mut Context<Self>) + 'static>,
 }
 impl<T> TabsView<T> {
@@ -40,6 +41,7 @@ impl<T> TabsView<T> {
             active_tab: 0,
             tabs: vec![None],
             scroll_handle: ScrollHandle::new(),
+            latest_scroll_offset: Point::default(),
             on_tab_changed: Box::new(|_window, _cx| {}),
         }
     }
@@ -72,12 +74,16 @@ impl<T> TabsView<T> {
         (self.on_tab_changed)(window, cx);
     }
 
-    pub fn scroll_to_active_tab(&mut self) {
+    pub fn scroll_to_active_tab(&mut self, _window: &mut Window, _cx: &mut Context<Self>) {
         if self.active_tab == 0 {
             self.scroll_handle.set_offset(Point::default());
         } else {
             self.scroll_handle.scroll_to_item(self.active_tab);
         }
+        self.save_latest_scroll();
+    }
+    fn save_latest_scroll(&mut self) {
+        self.latest_scroll_offset = self.scroll_handle.offset();
     }
     pub fn active_tab(&self) -> usize {
         self.active_tab
@@ -103,7 +109,7 @@ impl<T> TabsView<T> {
         cx: &mut Context<Self>,
     ) {
         self.remove_tab(self.active_tab, window, cx);
-        self.scroll_to_active_tab();
+        self.scroll_to_active_tab(window, cx);
         cx.notify();
     }
     pub fn on_action_create_tab(
@@ -113,91 +119,113 @@ impl<T> TabsView<T> {
         cx: &mut Context<Self>,
     ) {
         self.create_tab(None, window, cx);
-        self.scroll_to_active_tab();
+        self.scroll_to_active_tab(window, cx);
         cx.notify();
     }
     pub fn on_action_next_tab(&mut self, _: &NextTab, window: &mut Window, cx: &mut Context<Self>) {
         self.set_active_tab(self.active_tab + 1, window, cx);
-        self.scroll_to_active_tab();
+        self.scroll_to_active_tab(window, cx);
         cx.notify();
     }
     pub fn on_action_prev_tab(&mut self, _: &PrevTab, window: &mut Window, cx: &mut Context<Self>) {
         self.set_active_tab(self.active_tab.saturating_sub(1), window, cx);
-        self.scroll_to_active_tab();
+        self.scroll_to_active_tab(window, cx);
         cx.notify();
     }
 }
 impl<T: TabData> Render for TabsView<T> {
     fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let new_tab_button = if self.tabs.is_empty() {
+            Empty.into_any_element()
+        } else {
+            div()
+                .absolute()
+                .top_0()
+                .right_0()
+                .bottom_0()
+                .child(
+                    Button::new("button-new-tab-pdf")
+                        .icon(Icon::new(IconName::Plus))
+                        .on_click(cx.listener(|view, _, window, cx| {
+                            view.create_tab(None, window, cx);
+                            view.scroll_to_active_tab(window, cx);
+                        })),
+                )
+                .flex_shrink_0()
+                .into_any_element()
+        };
+        let tab_bar = TabBar::new("dynamic-tabs-with-pdf-files")
+            .with_menu(self.tabs.len() > 1)
+            .selected_index(self.active_tab)
+            .track_scroll(&self.scroll_handle)
+            .on_click(cx.listener(|view, index, window, cx| {
+                view.set_active_tab(*index, window, cx);
+                view.scroll_to_active_tab(window, cx);
+            }))
+            .children(self.tabs.iter().enumerate().map(|(tab_index, tab_data)| {
+                Tab::new(if let Some(tab_data) = tab_data {
+                    tab_data.label()
+                } else {
+                    "New tab".into()
+                })
+                .on_any_mouse_down(cx.listener(move |view, event, window, cx| {
+                    if let MouseDownEvent {
+                        button: MouseButton::Middle,
+                        ..
+                    } = event
+                    {
+                        view.remove_tab(tab_index, window, cx);
+                        view.scroll_to_active_tab(window, cx);
+                    }
+                }))
+            }));
+
         div()
             .flex()
-            .flex_row_reverse()
+            .flex_row()
+            .w_full()
+            .min_h(px(33.))
             .refine_style(&StyleRefinement {
                 align_items: Some(AlignItems::Stretch),
                 ..Default::default()
             })
-            .on_scroll_wheel(cx.listener(|view, event: &ScrollWheelEvent, window, cx| {
-                if event.control {
-                    match event.delta.pixel_delta(px(4.0)).y.cmp(&px(0.)) {
-                        Ordering::Less => {
-                            view.set_active_tab(view.active_tab.saturating_sub(1), window, cx);
-                            view.scroll_to_active_tab();
-                        }
-                        Ordering::Equal => {}
-                        Ordering::Greater => {
-                            view.set_active_tab(view.active_tab.saturating_add(1), window, cx);
-                            view.scroll_to_active_tab();
-                        }
-                    }
-                } else {
-                    let mut new_offset =
-                        view.scroll_handle.offset() + event.delta.pixel_delta(px(4.));
-                    new_offset.x = new_offset.x.min(view.scroll_handle.max_offset().width);
-                    new_offset.y = new_offset.y.min(view.scroll_handle.max_offset().height);
-                    view.scroll_handle.set_offset(new_offset);
-                }
-            }))
-            .child(if self.tabs.is_empty() {
-                Empty.into_any_element()
-            } else {
-                Button::new("button-new-tab-pdf")
-                    .icon(Icon::new(IconName::Plus))
-                    .on_click(cx.listener(|view, _, window, cx| {
-                        view.create_tab(None, window, cx);
-                        view.scroll_to_active_tab();
-                    }))
-                    .into_any_element()
-            })
             .child(
-                div().size_full().child(
-                    TabBar::new("dynamic-tabs-with-pdf-files")
-                        .with_menu(self.tabs.len() > 1)
-                        .selected_index(self.active_tab)
-                        .track_scroll(&self.scroll_handle)
-                        .on_click(cx.listener(|view, index, window, cx| {
-                            view.set_active_tab(*index, window, cx);
-                            view.scroll_to_active_tab();
-                        }))
-                        .children(self.tabs.iter().enumerate().map(|(tab_index, tab_data)| {
-                            Tab::new(if let Some(tab_data) = tab_data {
-                                tab_data.label()
-                            } else {
-                                "New tab".into()
-                            })
-                            .on_any_mouse_down(cx.listener(
-                                move |view, event, window, cx| {
-                                    if let MouseDownEvent {
-                                        button: MouseButton::Middle,
-                                        ..
-                                    } = event
-                                    {
-                                        view.remove_tab(tab_index, window, cx);
-                                        view.scroll_to_active_tab();
-                                    }
-                                },
-                            ))
-                        })),
-                ),
+                div()
+                    .absolute()
+                    .top_0()
+                    .left_0()
+                    .right_8()
+                    .on_scroll_wheel(cx.listener(|view, event: &ScrollWheelEvent, window, cx| {
+                        let prev_offset = view.latest_scroll_offset;
+                        view.save_latest_scroll();
+
+                        if event.control {
+                            view.scroll_handle.set_offset(prev_offset);
+                            view.save_latest_scroll();
+
+                            match event.delta.pixel_delta(px(1.0)).y.cmp(&px(0.)) {
+                                Ordering::Greater => {
+                                    view.set_active_tab(
+                                        view.active_tab.saturating_sub(1),
+                                        window,
+                                        cx,
+                                    );
+                                    view.scroll_to_active_tab(window, cx);
+                                }
+                                Ordering::Equal => {}
+                                Ordering::Less => {
+                                    view.set_active_tab(
+                                        view.active_tab.saturating_add(1),
+                                        window,
+                                        cx,
+                                    );
+                                    view.scroll_to_active_tab(window, cx);
+                                }
+                            }
+                        }
+                    }))
+                    .child(tab_bar),
             )
+            .child(new_tab_button)
     }
 }
