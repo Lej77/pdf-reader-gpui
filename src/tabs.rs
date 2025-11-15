3e7358cc919acbe7ed376d1cd1cbadacbbe19980
Lej77 <31554212+Lej77@users.noreply.github.com>
2025-11-15 04:59:59 +0100

    feat: smooth scrolling of tab bar

--- src/tabs.rs
+++ src/tabs.rs
@@ -2,18 +2,17 @@ use gpui::prelude::FluentBuilder;
 use gpui::{
     AlignItems, Context, Empty, InteractiveElement, IntoElement, MouseButton, MouseDownEvent,
     ParentElement, Pixels, Point, Render, ScrollHandle, ScrollWheelEvent, SharedString,
-    StatefulInteractiveElement, StyleRefinement, Styled, Window, div, px,
+    StatefulInteractiveElement, StyleRefinement, Styled, Window, div, point, px,
 };
 use gpui_component::button::Button;
 use gpui_component::tab::{Tab, TabBar};
 use gpui_component::tooltip::Tooltip;
 use gpui_component::{Icon, IconName, StyledExt};
-use std::cell::Cell;
 use std::cmp::Ordering;
+use std::ops::Sub;
 use std::path::PathBuf;
-use std::rc::Rc;
 use std::sync::Arc;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 #[derive(Clone, PartialEq, Default, Debug, gpui::Action)]
 #[action(namespace = tabs)]
@@ -31,6 +30,174 @@ pub struct NextTab;
 #[action(namespace = tabs)]
 pub struct PrevTab;
 
+pub struct SmoothScrollState {
+    /// Animation state
+    animating: bool,
+    /// The scroll offset where the animation started.
+    start_offset: Point<Pixels>,
+    /// The last scroll offset acknowledged by this smooth scroll state.
+    last_set_offset: Point<Pixels>,
+    /// The scroll offset that the animation will finish at.
+    target_offset: Point<Pixels>,
+    /// Animation started at this time. Note that if a new target offset is provided during the
+    /// animation then this time might be recalculated to provide a smooth animation.
+    start_time: Instant,
+    /// Animation duration
+    duration: Duration,
+    /// True if one of the `scroll_to_` method on the scroll handle was called, for example
+    /// [`ScrollHandle::scroll_to_item`]. These don't set the offset until 2 frames after
+    /// requested so we need to request a new update then to get and override that new offset.
+    requested_async_scroll: u32,
+}
+impl SmoothScrollState {
+    pub fn new() -> Self {
+        Self {
+            animating: false,
+            start_offset: point(px(0.), px(0.)),
+            last_set_offset: point(px(0.), px(0.)),
+            target_offset: point(px(0.), px(0.)),
+            start_time: Instant::now(),
+            duration: Duration::from_millis(1500),
+            requested_async_scroll: 0,
+        }
+    }
+
+    // Easing function (ease-in-out)
+    fn ease_in_out(t: f32) -> f32 {
+        if t < 0.5 {
+            2.0 * t * t
+        } else {
+            -1.0 + (4.0 - 2.0 * t) * t
+        }
+    }
+
+    fn bound_scroll(scroll_handle: &ScrollHandle, offset: Point<Pixels>) -> Point<Pixels> {
+        let bounds = scroll_handle.max_offset();
+        let safe_x_range = (-bounds.width).min(px(0.0))..px(0.);
+        let safe_y_range = (-bounds.height).min(px(0.0))..px(0.);
+        point(
+            offset.x.clamp(safe_x_range.start, safe_x_range.end),
+            offset.y.clamp(safe_y_range.start, safe_y_range.end),
+        )
+    }
+
+    /// Indicate that the newly set scroll offset is where we actually want to scroll.
+    pub fn requested_async_scroll(&mut self) {
+        self.requested_async_scroll = 2;
+    }
+    /// Start animation if target offset has changed.
+    pub fn noticed_scroll(&mut self, scroll_handle: &ScrollHandle) {
+        let current_offset = Self::bound_scroll(scroll_handle, scroll_handle.offset());
+        let diff = self.last_set_offset - current_offset;
+        if diff.x.abs() > px(2.) || diff.y.abs() > px(2.) {
+            self.start_offset = self.wanted_offset();
+            self.start_scroll_to(current_offset);
+        }
+    }
+    /// Assume that the offset was changed relative to the last set offset.
+    pub fn noticed_scroll_wheel_event<T: 'static>(
+        &mut self,
+        scroll_handle: &ScrollHandle,
+        _window: &mut Window,
+        _cx: &mut Context<T>,
+    ) {
+        let current_offset = Self::bound_scroll(&scroll_handle, scroll_handle.offset());
+
+        if self.last_set_offset != current_offset {
+            self.start_offset = self.wanted_offset();
+            self.start_scroll_to(Self::bound_scroll(
+                scroll_handle,
+                self.target_offset + (current_offset - self.last_set_offset),
+            ));
+        }
+        self.last_set_offset = current_offset;
+    }
+    /// Start animation
+    pub fn start_scroll_to(&mut self, target_offset: Point<Pixels>) {
+        if target_offset == self.target_offset {
+            return;
+        }
+        // self.start_offset = start_offset;
+        self.target_offset = target_offset;
+        if self.animating {
+            // Select a start time that gives the same progress percentage in order to not change
+            // the animation "speed".
+            let elapsed = Instant::now().duration_since(self.start_time);
+            let mut progress = (elapsed.as_secs_f32() / self.duration.as_secs_f32()).min(1.0);
+            // If more than half has passed then consider an earlier animation point with same speed
+            // (i.e. 90% of progress has same speed as 10% of progress)
+            if progress > 0.5 {
+                progress = 1. - progress;
+            }
+            self.duration = Duration::from_millis(300);
+            // Ensure at least half the time remains:
+            self.start_time = Instant::now().sub(Duration::from_secs_f32(
+                self.duration.as_secs_f32() * progress,
+            ));
+        } else {
+            self.duration = Duration::from_millis(300);
+            self.start_time = Instant::now();
+        }
+        self.animating = true;
+    }
+
+    pub fn is_complete(&self) -> bool {
+        Instant::now().duration_since(self.start_time) >= self.duration
+    }
+
+    pub fn is_animating(&self) -> bool {
+        self.animating
+    }
+
+    /// Gets the desired offset for the current time. If animating then this will calculate an
+    /// interpolated offset
+    pub fn wanted_offset(&self) -> Point<Pixels> {
+        if !self.animating {
+            return self.target_offset;
+        }
+        let elapsed = Instant::now().duration_since(self.start_time);
+        if elapsed >= self.duration {
+            self.target_offset
+        } else {
+            let progress = (elapsed.as_secs_f32() / self.duration.as_secs_f32()).min(1.0);
+            let eased = Self::ease_in_out(progress);
+
+            point(
+                self.start_offset.x + (self.target_offset.x - self.start_offset.x) * eased,
+                self.start_offset.y + (self.target_offset.y - self.start_offset.y) * eased,
+            )
+        }
+    }
+
+    pub fn preform_scroll<T: 'static>(
+        &mut self,
+        scroll_handle: &ScrollHandle,
+        window: &mut Window,
+        cx: &mut Context<T>,
+    ) {
+        if self.requested_async_scroll > 0 {
+            self.requested_async_scroll -= 1;
+            self.noticed_scroll(scroll_handle);
+            window.request_animation_frame();
+        }
+        // Update animation if active
+        if self.animating {
+            let next_offset = self.wanted_offset();
+            scroll_handle.set_offset(next_offset);
+            self.last_set_offset = next_offset;
+            if self.is_complete() {
+                // Animation complete
+                self.animating = false;
+            } else {
+                // Request next frame (pattern from scrollbar fade animation)
+                window.request_animation_frame();
+            }
+
+            cx.notify();
+        }
+    }
+}
+
 pub trait TabData: 'static {
     fn label(&self) -> SharedString;
     fn full_path(&self) -> Arc<PathBuf>;
@@ -40,7 +207,7 @@ pub struct TabsView<T: 'static> {
     active_tab: usize,
     tabs: Vec<Option<T>>,
     scroll_handle: ScrollHandle,
-    latest_scroll_offset: Rc<Cell<Point<Pixels>>>,
+    smooth_scroll: SmoothScrollState,
     on_tab_changed: Box<dyn Fn(&mut Window, &mut Context<Self>) + 'static>,
 }
 impl<T> TabsView<T> {
@@ -49,7 +216,7 @@ impl<T> TabsView<T> {
             active_tab: 0,
             tabs: vec![None],
             scroll_handle: ScrollHandle::new(),
-            latest_scroll_offset: Rc::new(Cell::new(Point::default())),
+            smooth_scroll: SmoothScrollState::new(),
             on_tab_changed: Box::new(|_window, _cx| {}),
         }
     }
@@ -73,42 +240,29 @@ impl<T> TabsView<T> {
                 self.tabs.remove(index);
                 if self.active_tab == index {
                     self.set_active_tab(index, window, cx);
+                    self.scroll_to_active_tab(window, cx); // ensure the new tab is visible
                 } else if index < self.active_tab {
                     self.active_tab -= 1;
                 }
             }
         }
+        cx.notify();
     }
     pub fn set_active_tab(&mut self, index: usize, window: &mut Window, cx: &mut Context<Self>) {
         self.active_tab = index.min(self.tabs.len().saturating_sub(1));
         (self.on_tab_changed)(window, cx);
     }
 
-    pub fn scroll_to_active_tab(&mut self, window: &mut Window, cx: &mut Context<Self>) {
+    pub fn scroll_to_active_tab(&mut self, _window: &mut Window, _cx: &mut Context<Self>) {
         if self.active_tab == 0 {
             self.scroll_handle.set_offset(Point::default());
+            self.smooth_scroll.noticed_scroll(&self.scroll_handle);
         } else {
-            self.scroll_handle.scroll_to_item(self.active_tab);
+            self.scroll_handle.scroll_to_item(self.active_tab); // <- updates the scroll offset later
         }
-        self.save_latest_scroll(window, cx);
-    }
-    fn save_latest_scroll(&mut self, _window: &mut Window, cx: &mut Context<Self>) {
-        self.latest_scroll_offset.set(self.scroll_handle.offset());
 
-        cx.foreground_executor()
-            .spawn({
-                let background = cx.background_executor().clone();
-                let latest_scroll_offset = self.latest_scroll_offset.clone();
-                let scroll_handle = self.scroll_handle.clone();
-                async move {
-                    // TODO: perf - only check after actions have been taken
-                    loop {
-                        background.timer(Duration::from_millis(100)).await;
-                        latest_scroll_offset.set(scroll_handle.offset());
-                    }
-                }
-            })
-            .detach();
+        // We need to get the scroll offset when it becomes available next frame:
+        self.smooth_scroll.requested_async_scroll();
     }
     pub fn active_tab(&self) -> usize {
         self.active_tab
@@ -134,8 +288,6 @@ impl<T> TabsView<T> {
         cx: &mut Context<Self>,
     ) {
         self.remove_tab(self.active_tab, window, cx);
-        self.scroll_to_active_tab(window, cx);
-        cx.notify();
     }
     pub fn on_action_create_tab(
         &mut self,
@@ -159,7 +311,11 @@ impl<T> TabsView<T> {
     }
 }
 impl<T: TabData> Render for TabsView<T> {
-    fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+    fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        self.smooth_scroll.noticed_scroll(&self.scroll_handle);
+        self.smooth_scroll
+            .preform_scroll(&self.scroll_handle, window, cx);
+
         let new_tab_button = if self.tabs.is_empty() {
             Empty.into_any_element()
         } else {
@@ -207,7 +363,6 @@ impl<T: TabData> Render for TabsView<T> {
                             } = event
                             {
                                 view.remove_tab(tab_index, window, cx);
-                                view.scroll_to_active_tab(window, cx);
                             }
                         })),
                 )
@@ -217,7 +372,6 @@ impl<T: TabData> Render for TabsView<T> {
                         .on_click(cx.listener(move |view, _event, window, cx| {
                             cx.stop_propagation();
                             view.remove_tab(tab_index, window, cx);
-                            view.scroll_to_active_tab(window, cx);
                         }))
                         .max_w_6()
                         .max_h_6(),
@@ -246,35 +400,48 @@ impl<T: TabData> Render for TabsView<T> {
                     .flex_1()
                     .left_0()
                     .overflow_hidden()
-                    .on_scroll_wheel(cx.listener(|view, event: &ScrollWheelEvent, window, cx| {
-                        let prev_offset = view.latest_scroll_offset.get();
-                        view.save_latest_scroll(window, cx);
+                    .child(
+                        div()
+                            .absolute()
+                            .bottom_0()
+                            .top_0()
+                            .right_0()
+                            .left_0()
+                            .on_scroll_wheel(cx.listener(
+                                |view, event: &ScrollWheelEvent, window, cx| {
+                                    if event.control {
+                                        view.scroll_handle
+                                            .set_offset(view.smooth_scroll.wanted_offset());
 
-                        if event.control {
-                            view.scroll_handle.set_offset(prev_offset);
-                            view.save_latest_scroll(window, cx);
-
-                            match event.delta.pixel_delta(px(1.0)).y.cmp(&px(0.)) {
-                                Ordering::Greater => {
-                                    view.set_active_tab(
-                                        view.active_tab.saturating_sub(1),
-                                        window,
-                                        cx,
-                                    );
-                                    view.scroll_to_active_tab(window, cx);
-                                }
-                                Ordering::Equal => {}
-                                Ordering::Less => {
-                                    view.set_active_tab(
-                                        view.active_tab.saturating_add(1),
-                                        window,
-                                        cx,
-                                    );
-                                    view.scroll_to_active_tab(window, cx);
-                                }
-                            }
-                        }
-                    }))
+                                        match event.delta.pixel_delta(px(1.0)).y.cmp(&px(0.)) {
+                                            Ordering::Greater => {
+                                                view.set_active_tab(
+                                                    view.active_tab.saturating_sub(1),
+                                                    window,
+                                                    cx,
+                                                );
+                                                view.scroll_to_active_tab(window, cx);
+                                            }
+                                            Ordering::Equal => {}
+                                            Ordering::Less => {
+                                                view.set_active_tab(
+                                                    view.active_tab.saturating_add(1),
+                                                    window,
+                                                    cx,
+                                                );
+                                                view.scroll_to_active_tab(window, cx);
+                                            }
+                                        }
+                                    } else {
+                                        view.smooth_scroll.noticed_scroll_wheel_event(
+                                            &view.scroll_handle,
+                                            window,
+                                            cx,
+                                        );
+                                    }
+                                },
+                            )),
+                    )
                     .child(tab_bar),
             )
     }
